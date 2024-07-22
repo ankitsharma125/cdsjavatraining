ab51e9946bfd5903773d0485c3cad3ec4743be22
ankitsharma125 <aankitsharma25@gmail.com>
2024-07-22 13:57:20 +0530

    swap java file

---
+++ code.java
@@ -0,0 +1,12 @@
+public class Findlength {
+    public static void main(String[] args)
+    {
+        String name = "AnkitSharma";
+        int count= 0;
+        for(int i=0; i< name.length(); i++){
+            count++;
+        }
+        System.out.println("length of the string is " + count);
+    }
+       
+}
---
+++ swap.java
@@ -0,0 +1,13 @@
+public class SwapNumber {
+    public static void main(String[] args)
+    {
+        int a = 15, b = 10, c;
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("After swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
