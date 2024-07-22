06acdbf5d91779ef1c7c0c2806f1923f14b7d6b9
ankitsharma125 <aankitsharma25@gmail.com>
2024-07-22 15:16:37 +0530

    sum of two number

---
+++ sum.java
@@ -0,0 +1,9 @@
+// write a program to add two numbers
+class Add {
+    public static void main(String[] args) {
+        int a=5;
+        float b= 7.5f;
+        float add = a + b;
+        System.out.println("sum is :" + add);
+    }
+}
